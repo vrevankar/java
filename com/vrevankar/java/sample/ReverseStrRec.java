177c0689eeb090f7ac01aab951b0b254cf6ba051
vrevankar <vinay.revankar@gmail.com>
2017-02-26 16:17:45 -0800

    Reverse a string using recursion

---
+++ com/vrevankar/java/sample/ReverseStrRec.java
@@ -0,0 +1,18 @@
+// reverse a String using recursion
+package com.vrevankar.java.sample;
+
+import java.util.Scanner;
+
+public class ReverseStrRec {
+  public static void main (String[] args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Please enter the String: ");
+    System.out.println("Reverse String: " + new ReverseStrRec().reverseStr(scanner.nextLine()));
+  }
+
+  public String reverseStr(String str){
+    if (str.length() == 0) return str;
+    return reverseStr(str.substring(1)) + str.charAt(0);
+  }
+
+}
